36d19bd54bd0f32b6f035f52dfd7a74e40284e3d
napi <root@localhost>
2022-04-04 12:08:13 +0200

    change auth method from user/pass to sessionid; save gpx based on activity type for strava

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # RunLogGpxExporter
 
 Gpx files exporter from https://run-log.com/ site
+
+./python3 run_log_exporter.py <login> <sessionid>
+
+<sessionid> login in browser, look for sessionid cookie value.
--- run_log_exporter.py
+++ run_log_exporter.py
@@ -5,10 +5,9 @@ from itertools import chain
 
 RUNLOG_URL = 'https://run-log.com/'
 
-def open_run_log_session(user, passwd):
+def open_run_log_session(user,sessionid):
     run_log_session = requests.session()
-    run_log_session.post(RUNLOG_URL + 'account/login/', data={'username': user,
-                                                              'password': passwd})
+    run_log_session.cookies.set("sessionid", sessionid, domain="run-log.com")
     return run_log_session
 
 
@@ -45,6 +44,10 @@ def get_date(page):
     date = re.findall('Data:</span><span class="value">\d+-\d+-\d+', page.text)[0]
     return date.replace('Data:</span><span class="value">', '')
 
+def get_workout_type(page):
+    workout_type = re.findall('<h2>Aktywność: \w+', page.text)[0]
+    return workout_type.replace('<h2>Aktywność: ', '')
+            
 
 def gpx_ids(session, workouts):
     url_template = RUNLOG_URL + 'workout/workout_show/{}'
@@ -54,37 +57,52 @@ def gpx_ids(session, workouts):
         page_code = session.get(url_template.format(workout_id))
         print('{}/{}'.format(count + 1, len(workouts)))
         try:
-            workout = get_id(page_code), get_date(page_code)
+            workout = get_id(page_code), get_date(page_code), get_workout_type(page_code)
             ids.append(workout)
         except:
             print('No gpx found for workout id {}!'.format(workout_id))
             pass
+    print(ids)
     return ids
 
 
-def save_gpx(gpx, id, day):
-    with open("dupa/{}_{}.gpx".format(day, id), "w") as f:
+def save_gpx(gpx, id, day, workout_type):
+    with open("{}_{}_{}.gpx".format(day, id, workout_type), "w") as f:
         f.write(gpx)
 
 
 def download_gpxies(session, ids):
-    url_template = RUNLOG_URL + 'tracks/export_workout_track/Rysmen/{}/gpx'
+    url_template = RUNLOG_URL + 'tracks/export_workout_track/'+username+'/{}/gpx'
     def get_gpx(id):
         return session.get(url_template.format(id)).text
     def correct_dates(gpx):
         return re.sub('\d+-\d+-\d+', day, gpx)
     def fill_activity_type(gpx):
-        return re.sub('<trk><trkseg>', '<trk><type>RUNNING</type><trkseg>', gpx)
-    for count, (id, day) in enumerate(ids):
+        STRAVA_WORKOUT_TYPE = "WORKOUT"
+        if workout_type == "Spacer":
+            STRAVA_WORKOUT_TYPE = "WALK"
+        if workout_type == "Rower":
+            STRAVA_WORKOUT_TYPE = "RIDE"
+        if workout_type == "Bieganie" or workout_type == "Running" or workout_type == "Trening" or workout_type == "Bieg":
+            STRAVA_WORKOUT_TYPE = "RUN"
+        if workout_type == "Basen":
+            STRAVA_WORKOUT_TYPE = "SWIM"
+        if workout_type == "Siłownia":
+            STRAVA_WORKOUT_TYPE = "WORKOUT"
+        if workout_type == "Rower stacjonarny":
+            STRAVA_WORKOUT_TYPE = "VIRTUALRIDE"
+        return re.sub('<trk><trkseg>', '<trk><type>'+STRAVA_WORKOUT_TYPE+'</type><trkseg>', gpx)
+    for count, (id, day, workout_type) in enumerate(ids):
         print("Downloading gpx: {} {}/{}".format(id, count + 1, len(ids)))
         gpx = fill_activity_type(correct_dates(get_gpx(id)))
-        save_gpx(gpx, id, day)
+        save_gpx(gpx, id, day, workout_type)
 
 
 username = sys.argv[1]
-password = sys.argv[2]
+sessionid = sys.argv[2]
 
-run_log_session = open_run_log_session(username, password)
+run_log_session = open_run_log_session(username,sessionid)
 ids = gpx_ids(run_log_session, workout_ids(run_log_session, get_num_of_pages(run_log_session)))
 #ids = gpx_ids(run_log_session, workout_ids(run_log_session, 1))
 download_gpxies(run_log_session, ids)
+
